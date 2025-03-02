7952d432971eebce429682dc9a69542548855c47
sandeepbhimisetty <322103383012@gvpce.ac.in>
2025-03-02 15:44:49 +0530

    Updated templates and fixed missing files issue

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let button = document.querySelector("button");
-    button.addEventListener("click", function() {
-        button.innerText = "Calculating...";
-    });
-});
- 
